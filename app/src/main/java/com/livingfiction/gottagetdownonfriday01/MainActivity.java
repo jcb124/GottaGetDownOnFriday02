76ae765d49c1eafd95faf06a9563829931a93f8b
dan9418 <dan9418@hotmail.com>
2016-02-20 11:47:09 -0500

    Added LoginButton, DoSomething Button, and ResultTextView

--- app/src/main/java/com/livingfiction/gottagetdownonfriday01/MainActivity.java
+++ app/src/main/java/com/livingfiction/gottagetdownonfriday01/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -21,7 +22,9 @@ import com.facebook.login.widget.LoginButton;
 public class MainActivity extends AppCompatActivity {
 
     private TextView info;
+    private TextView result;
     private LoginButton loginButton;
+    private Button doButton;
     private CallbackManager callbackManager;
 
     @Override
@@ -32,13 +35,15 @@ public class MainActivity extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
         info = (TextView)findViewById(R.id.info);
+        result = (TextView)findViewById(R.id.result);
+        doButton = (Button) findViewById(R.id.doSomething);
         loginButton = (LoginButton)findViewById(R.id.login_button);
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 info.setText(R.string.login_success);
-                //"User ID: " + loginResult.getAccessToken().getUserId() + "\n" + "Auth Token: " + loginResult.getAccessToken().getToken()
             }
 
             @Override
@@ -51,11 +56,16 @@ public class MainActivity extends AppCompatActivity {
                 info.setText(R.string.login_fail);
             }
         });
+
+        doButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                result.setText("I don't know, what do YOU want to do tonight?");
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
 }
